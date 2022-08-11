b155527da3c1ad84aa31bce6e53c0cbd0be728e6
Kandace Bishop <kbishop1@mail.sfsu.edu>
2022-08-10 19:04:40 -0700

    pushing spring project

---
+++ reviewapp/src/test/java/project2/reviewapp/ReviewappApplicationTests.java
@@ -0,0 +1,13 @@
+package project2.reviewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
