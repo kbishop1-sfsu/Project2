c64013e00ac000d65886a25de8504a562dc6a6ed
Jamison Mcqueary <jamison.mcqueary@comcast.net>
2022-08-12 11:50:28 -0400

    create tables added FKs

--- Tables/tablesProject2.sql
+++ Tables/tablesProject2.sql
@@ -2,9 +2,10 @@
 create database project2;
 
 --Creating user table
-create table if not exists user(
+create table if not exists users(
 	id serial primary key,
 	email varchar(40) unique not null,
+	username varchar(40) unique not null,
 	password varchar(30) not null,
 	firstName varchar(30) default null,
 	lastName varchar(30) default null,
@@ -13,19 +14,25 @@ create table if not exists user(
 );
 
 --Creating restaurant table 
-create table if not exists restaurant(
+create table if not exists restaurants(
 	id serial primary key,
-	name varchar(40) not null,
-	--fk for location id
+	restr_name varchar(40) not null,
+	locationID int,
+	category varchar(40),
+	website varchar(100),
+	constraint fk_locationID  --fk for locations id
+		foreign key(locationID)
+			references locations(id)
+			on delete cascade,
 );
 
 --Creating location table
-create table if not exists location(
+create table if not exists locations(
 	id serial primary key,
-	address varchar(60) not null,
+	street_addr varchar(60) not null,
 	city varchar(60) not null,
 	state varchar(20) not null,
-	zipCode numeric not null
+	zip int not null
 );
 
 --Creating review table
@@ -33,13 +40,78 @@ create table if not exists location(
 --A restaurant can have many reviews, review only has one restaurant
 create table if not exists review(
 	id serial primary key,
-	rating numeric check (0 <= rating >= 5), --rating should be between 0-5
-	description varchar(500) --is 500 big enough?
-	--fk user_id
-	--fk restaurant_id 
+	rating int check (0 <= rating >= 5), --rating should be between 0-5
+	posted timestamp,
+	description varchar(500),
+	userID int,
+	restID int,
+	constraint fk_user_review --fk users
+		foreign key(userID)
+			references users(id)
+			on delete cascade,
+	constraint fk_rest_review --fk restaurants
+		foreign key(restID)
+			references restaurants(id)
+			on delete cascade 
 );
 
 
 --Creating reservation table
-create table if not exists reservation();
+create table if not exists reservations(
+	id serial primary key,
+	resv_date timestamp,
+	status varchar(20) default "pending",
+	userID int,
+	restID int,
+	constraint fk_user_res --fk users
+		foreign key(userID)
+			references users(id)
+			on delete cascade,
+	constraint fk_rest_res --fk restaurants
+		foreign key(restID)
+			references restaurants(id)
+			on delete cascade
+);
+
+--Creating pages table
+create table if not exists pages(
+	id serial primary key,
+	avg_rating float,
+	restID int,
+	reviewID int,
+	reservationID int, 
+	constraint fk_rest_pages --fk restaurants
+		foreign key(restID)
+			references restaurants(id)
+			on delete cascade,
+	constraint fk_review_pages --fk reviews
+		foreign key(reviewID)
+			references reviews(id)
+			on delete cascade,
+	constraint fk_reservation_pages --fk reservations
+		foreign key(reservationID)
+			references reservations(id)
+			on delete cascade
+);
+
+--Creating admin table
+create table if not exists admins(
+	
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
