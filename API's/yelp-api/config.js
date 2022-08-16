1fbae6462c44487740f30287c694f2cafd8bc8d2
Mason-Rep <108552287+Mason-Rep@users.noreply.github.com>
2022-08-15 23:35:32 -0700

    making the search function for the yelp api

---
+++ API's/yelp-api/config.js
@@ -0,0 +1,3 @@
+export const API_BASE_URL = 'https://api.yelp.com/v3';
+
+export const BEARER_TOKEN = 'Txd3eOxcK2D3MWKr6-a3Z0UlYBjytGSIkTuKiibJ09r2MVABDmqypZ0IaNB_EvlXxkWJHLm4GKoXviFJXYtt2pEhbOE5-xKraKVjFQaGu9kHPlZCtNQakXZJrj76YnYx';
