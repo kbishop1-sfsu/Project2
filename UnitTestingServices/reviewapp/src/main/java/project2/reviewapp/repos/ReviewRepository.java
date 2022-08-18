5f91e287bf596cce2f3094ef88c2a13056c1e7b9
Jamison McQueary <108553056+JamisonMcq@users.noreply.github.com>
2022-08-18 19:42:46 -0400

    Delete UnitTestingServices/reviewapp directory

--- UnitTestingServices/reviewapp/src/main/java/project2/reviewapp/repos/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package project2.reviewapp.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import project2.reviewapp.models.Review;
-
-@Repository
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
-}
