db4f586a3b89bfea2b60578b7505aff2db4fe9fe
Jamison McQueary <108553056+JamisonMcq@users.noreply.github.com>
2022-08-17 21:46:27 -0400

    Delete reviewapp directory

--- reviewapp/src/main/java/project2/reviewapp/repos/RestaurantRepository.java
+++
@@ -1,9 +0,0 @@
-package project2.reviewapp.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import project2.reviewapp.models.Restaurant;
-
-@Repository
-public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
-}
