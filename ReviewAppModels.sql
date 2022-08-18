dae55c73084c0bf778be23cb0cf37aaddc96132e
Kandace Bishop <kbishop1@mail.sfsu.edu>
2022-08-18 11:07:07 -0700

    pushing sql and xml files

--- ReviewAppModels.sql
+++ ReviewAppModels.sql
@@ -10,14 +10,14 @@ drop table if exists pages;
 drop table if exists admins;
 drop table if exists users cascade;
 
-drop table if exists restaurants;
+drop table if exists restaurants cascade;
 drop table if exists locations;
 
 
 
 create table if not exists locations(
 	id serial primary key,
-	street_addr varchar(50) not null,
+	street_addr varchar(50) not null unique,
 	city varchar(50) not null,
 	state varchar (20) not null,
 	zip int not null check(zip > 9999) --check that the zip is at least 5 digits
@@ -29,11 +29,12 @@ create table if not exists restaurants(
 	restr_name varchar(50) not null,
 	loc int default 0 unique,
 	category varchar(20),
-	--phone int,--check that phone is 10  digits
 	website varchar(50),--default google result of search
+	avg_rating float default 0.0,
 	constraint fk_loc
 		foreign key(loc)
 			references locations(id)
+				--on delete cascade
 		
 );
 
@@ -46,18 +47,6 @@ create table if not exists users(
 	_password varchar(30) not null
 );
 
-create table if not exists pages(
-	id serial primary key,
-	avg_rating float default 0.0,
-	--list of reviews
-	--list of reservations?
-	restr_id int default 0 unique,
-	constraint fk_restr
-		foreign key(restr_id)
-			references restaurants(id)
-	
-);
-
 
 create table if not exists admins(
 	id serial primary key,
@@ -66,16 +55,13 @@ create table if not exists admins(
 	email varchar(30) not null unique,
 	username varchar(30) not null unique,
 	_password varchar(30) not null,
-	page_id int default 0,
-	constraint fk_restr_page
-		foreign key(page_id)
-			references pages(id)
+	restr_id int default 0,
+	constraint fk_assoc_restr
+		foreign key(restr_id)
+			references restaurants(id)
+				--on delete cascade
 		
 );
-
-
-
-
 
 
 create table if not exists reviews(
@@ -87,21 +73,25 @@ create table if not exists reviews(
 	constraint fk_user
 		foreign key (user_id)
 			references users(id)
+				--on delete cascade
 );
 
 
 create table if not exists reservations(
 	id serial primary key,
 	resv_date timestamp not null,
-	status varchar(20) default 'pending',
+	--status varchar(20) default 'pending',doesnt need to resolved
+	num_of_ppl int default 2 check(num_of_ppl<=10),
 	restr_id int not null,
 	user_id int not null,
 	constraint fk_restr_resv
 		foreign key (restr_id)
 			references restaurants(id),
+				--on delete cascade,
 	constraint fk_user_resv
 		foreign key (user_id)
 			references users(id)
+				--on delete cascade
 );
 
 	
